20a26189c239681e3e6e4b5ec9deb4e5f73316a0
Davidleofp <65404234+Davidleofp@users.noreply.github.com>
2023-08-29 16:02:54 -0500

    Update Buscaminas.java

--- src/main/java/com/mycompany/buscaminas/Buscaminas.java
+++ src/main/java/com/mycompany/buscaminas/Buscaminas.java
@@ -6,6 +6,6 @@ package com.mycompany.buscaminas;
 public class Buscaminas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo");
     }
 }
