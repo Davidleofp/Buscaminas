88b90f808fea17e3b1fec51c91f9c44a129be8ed
Davidleofp <65404234+Davidleofp@users.noreply.github.com>
2023-08-30 11:27:41 -0500

    Update LogicaMinas.java

--- src/main/java/logica/LogicaMinas.java
+++ src/main/java/logica/LogicaMinas.java
@@ -1,7 +1,24 @@
-
 package logica;
 
 
 public class LogicaMinas {
     
+    private int fila;
+    private int columnas;
+    
+    public float getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+
+    public float getColumnas() {
+        return columnas;
+    }
+
+    public void setColumnas(int columnas) {
+        this.columnas = columnas;
+    }
 }
